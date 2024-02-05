6519a51c31a6242b5a1d54422753f1b88b018873
Emilio Quintana <emilioquintana90@gmail.com>
2024-02-05 21:18:38 +0800

    Add Material UI to the project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  gql,
+} from "@apollo/client";
+
 import App from "./components/App.tsx";
+
 import "./styles/index.css";
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000",
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query GetMenus {
+        menus {
+          id
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
 );
